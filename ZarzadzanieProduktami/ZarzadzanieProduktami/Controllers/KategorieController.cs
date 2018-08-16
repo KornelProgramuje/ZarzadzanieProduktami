ad6dc0457ec55c5cd3a30a227ea64ca9dcff93e8
KornelProgramuje <kornelpen@gmail.com>
2018-08-16 14:02:22 +0200

    Dodanie kontrolerów kategorii i produktów

---
+++ ZarzadzanieProduktami/ZarzadzanieProduktami/Controllers/KategorieController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ZarzadzanieProduktami.DAL;
+using ZarzadzanieProduktami.Models;
+
+namespace ZarzadzanieProduktami.Controllers
+{
+    public class KategorieController : Controller
+    {
+        private ProduktyContext db = new ProduktyContext();
+
+        // GET: Kategorie
+        public ActionResult Index()
+        {
+            return View(db.Kategorie.ToList());
+        }
+
+        // GET: Kategorie/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kategoria kategoria = db.Kategorie.Find(id);
+            if (kategoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kategoria);
+        }
+
+        // GET: Kategorie/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Kategorie/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,NazwaKategorii")] Kategoria kategoria)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Kategorie.Add(kategoria);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(kategoria);
+        }
+
+        // GET: Kategorie/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kategoria kategoria = db.Kategorie.Find(id);
+            if (kategoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kategoria);
+        }
+
+        // POST: Kategorie/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,NazwaKategorii")] Kategoria kategoria)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(kategoria).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(kategoria);
+        }
+
+        // GET: Kategorie/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kategoria kategoria = db.Kategorie.Find(id);
+            if (kategoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kategoria);
+        }
+
+        // POST: Kategorie/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Kategoria kategoria = db.Kategorie.Find(id);
+            db.Kategorie.Remove(kategoria);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ZarzadzanieProduktami/ZarzadzanieProduktami/Controllers/ProduktyController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ZarzadzanieProduktami.DAL;
+using ZarzadzanieProduktami.Models;
+
+namespace ZarzadzanieProduktami.Controllers
+{
+    public class ProduktyController : Controller
+    {
+        private ProduktyContext db = new ProduktyContext();
+
+        // GET: Produkty
+        public ActionResult Index()
+        {
+            var produkty = db.Produkty.Include(p => p.Kategorie);
+            return View(produkty.ToList());
+        }
+
+        // GET: Produkty/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produkt produkt = db.Produkty.Find(id);
+            if (produkt == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produkt);
+        }
+
+        // GET: Produkty/Create
+        public ActionResult Create()
+        {
+            ViewBag.KategoriaId = new SelectList(db.Kategorie, "Id", "NazwaKategorii");
+            return View();
+        }
+
+        // POST: Produkty/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nazwa,KategoriaId,Cena,Opis")] Produkt produkt)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Produkty.Add(produkt);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.KategoriaId = new SelectList(db.Kategorie, "Id", "NazwaKategorii", produkt.KategoriaId);
+            return View(produkt);
+        }
+
+        // GET: Produkty/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produkt produkt = db.Produkty.Find(id);
+            if (produkt == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.KategoriaId = new SelectList(db.Kategorie, "Id", "NazwaKategorii", produkt.KategoriaId);
+            return View(produkt);
+        }
+
+        // POST: Produkty/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nazwa,KategoriaId,Cena,Opis")] Produkt produkt)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(produkt).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.KategoriaId = new SelectList(db.Kategorie, "Id", "NazwaKategorii", produkt.KategoriaId);
+            return View(produkt);
+        }
+
+        // GET: Produkty/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produkt produkt = db.Produkty.Find(id);
+            if (produkt == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produkt);
+        }
+
+        // POST: Produkty/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Produkt produkt = db.Produkty.Find(id);
+            db.Produkty.Remove(produkt);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
