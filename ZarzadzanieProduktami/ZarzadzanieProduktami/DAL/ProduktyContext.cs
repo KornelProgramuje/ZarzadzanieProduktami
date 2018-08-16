e38f41644a384eb1a9df7e6559fd89ecc8939bb4
KornelProgramuje <kornelpen@gmail.com>
2018-08-16 13:56:19 +0200

    Utworzenie warstwy dostępu do danych(DAL) oraz connection string'a

---
+++ ZarzadzanieProduktami/ZarzadzanieProduktami/DAL/ProduktyContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using ZarzadzanieProduktami.Models;
+
+namespace ZarzadzanieProduktami.DAL
+{
+    public class ProduktyContext:DbContext
+    {
+        public ProduktyContext() : base("BazaProdukty") { }
+
+        public DbSet<Kategoria> Kategorie { get; set; }
+        public DbSet<Produkt> Produkty { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
