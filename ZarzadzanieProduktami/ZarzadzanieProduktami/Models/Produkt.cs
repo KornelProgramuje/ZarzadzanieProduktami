325ca12735833068fb3d88d6d3f1869559f670bd
KornelProgramuje <kornelpen@gmail.com>
2018-08-16 13:49:24 +0200

    Zdefiniowanie klas modelu

---
+++ ZarzadzanieProduktami/ZarzadzanieProduktami/Models/Produkt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZarzadzanieProduktami.Models
+{
+    public class Produkt
+    {
+        public int Id { get; set; }
+        public string Nazwa { get; set; }
+        public int KategoriaId { get; set; }
+        public string Cena { get; set; }
+        public string Opis { get; set; }
+
+
+        public virtual Kategoria Kategorie { get; set; }
+    }
+}
